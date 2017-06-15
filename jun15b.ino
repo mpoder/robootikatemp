362171b17c0c94c763d6165b2ebf4bcfc21ce5f3
Mihkel Põder <mpoder@users.noreply.github.com>
2017-06-15 14:17:36 +0300

    Lisatud ajalimiidiga vilkumine

--- jun15b.ino
+++ jun15b.ino
@@ -5,11 +5,38 @@ void setup() {
 
 }
 
+int counter;
+int blinker;
+
+
 void loop() {
   // put your main code here, to run repeatedly:
+  delay(15);
   if (digitalRead(2) == HIGH) {
-      digitalWrite(4, HIGH);
-    } else {
-      digitalWrite(4, LOW);
-      }
+    counter = (5 * 60);
+  } else {
+
+  }
+
+  if (counter > 0) {
+    blinkLight();
+  } else {
+    digitalWrite(4, LOW);
+  }
+
+  if (counter > 0) {
+    counter = counter - 1;
+  }
 }
+
+void blinkLight() {
+  blinker = blinker - 1;
+  if (blinker <= 0) {
+    blinker = 30;}
+  if (blinker > 15) {
+    digitalWrite(4, HIGH);
+    } else {
+    digitalWrite(4, LOW);
+    }
+  }
+
